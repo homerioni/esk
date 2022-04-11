57ba4b7e80be702944481cf8a148b37fa9b6d76c
homerioni <homerion13@gmail.com>
2022-04-11 15:52:44 +0300

    create section main-intro

--- js/swiper.js
+++ js/swiper.js
@@ -29,3 +29,45 @@ const rem = function (rem) {
         return (100/375) * (0.1 * $(window).width()) * rem;
     }
 }
+
+const main_intro_slider = new Swiper('.main-intro', {
+    direction: 'horizontal',
+    slidesPerView: 1,
+
+    pagination: {
+        el: '.main-intro__pagination',
+        type: 'fraction',
+        formatFractionCurrent: function (number) {
+            if (number < 10) {
+                return '0' + number;
+            } else {
+                return number;
+            }
+        },
+        formatFractionTotal: function (number) {
+            if (number < 10) {
+                return '0' + number;
+            } else {
+                return number;
+            }
+        },
+        renderFraction: function (currentClass, totalClass) {
+            return '<div class="' + currentClass + '"></div>' +
+                '<div class="main-intro__scrollbar"></div>' +
+                '<div class="' + totalClass + '"></div>';
+        },
+    },
+
+    navigation: {
+        nextEl: '.main-intro__arrow--right',
+        prevEl: '.main-intro__arrow--left',
+    },
+
+    scrollbar: {
+        el: ".main-intro__scrollbar",
+    },
+
+    autoplay: {
+        delay: 7000,
+    },
+});
