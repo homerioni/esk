4e7826acb9fbbadef8cb6c00f49b17e40976c990
homerioni <homerion13@gmail.com>
2022-04-13 20:47:51 +0300

    create modal-callback

--- js/main.js
+++ js/main.js
@@ -17,10 +17,30 @@ $(document).ready(function () {
         $('.header').addClass('search-open');
         $('.header__search').addClass('open');
     });
-
     $('.header__search-close').click(function () {
         $('.header').removeClass('search-open');
         $('.header__search').removeClass('open');
+    });
+
+    // Popup callback
+    $('.popup-callback').click(() => {
+        $('.modal-callback')
+            .css('display', 'flex')
+            .hide()
+            .fadeIn();
+    });
+    $('.modal-callback__form').submit(() => {
+        $('.modal-callback').addClass('second');
+    });
+    $('.modal-callback__close').click(() => {
+        $('.modal-callback')
+            .hide()
+            .removeClass('second');
+    });
+    $('.modal-callback__bg').click(() => {
+        $('.modal-callback')
+            .hide()
+            .removeClass('second');
     });
 
 });
